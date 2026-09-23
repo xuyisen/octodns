d24c88735d6162018d5f7ef998edc6799acecb89
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-23 16:40:56 -0400

    xuyisen__diff__id_60

--- tests/test_octodns_manager.py
+++ tests/test_octodns_manager.py
@@ -80,11 +80,13 @@ class TestManager(TestCase):
         self.assertTrue('missing sources' in str(ctx.exception))
 
     def test_missing_zone(self):
-        with self.assertRaises(ManagerException) as ctx:
-            Manager(get_config_filename('dynamic-config.yaml')).sync(
-                ['missing.zones.']
-            )
-        self.assertTrue('Requested zone:' in str(ctx.exception))
+        with TemporaryDirectory() as tmpdir:
+            environ['YAML_TMP_DIR'] = tmpdir.dirname
+            with self.assertRaises(ManagerException) as ctx:
+                Manager(get_config_filename('dynamic-config.yaml')).sync(
+                    ['missing.zones.']
+                )
+            self.assertTrue('Requested zone:' in str(ctx.exception))
 
     def test_missing_targets(self):
         with self.assertRaises(ManagerException) as ctx:
